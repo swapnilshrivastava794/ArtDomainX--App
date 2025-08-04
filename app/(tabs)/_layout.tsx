df8d8a2703afb4fb87e264a9d73002d3004ebbc1
rm2004-cyber <rahulagarwal200420@gmail.com>
2025-08-04 18:40:17 +0530

    rr

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,12 +1,17 @@
-// app/(tabs)/_layout.tsx
-import { Slot } from 'expo-router';
+import { Stack } from 'expo-router';
 import { View } from 'react-native';
 import BottomTabs from '../components/BottomTabs';
 
 export default function TabsLayout() {
   return (
     <View style={{ flex: 1 }}>
-      <Slot />
+      <Stack
+        screenOptions={{
+          headerShown: false,
+          animation: 'default', 
+        }}
+      >
+      </Stack>
       <BottomTabs />
     </View>
   );
